149c46681cacf69ee2dd2aa3a160a141d1a86791
Jieun <citty3993jin@gmail.com>
2026-03-18 15:29:52 +0900

    22159016_ShootingGame

---
+++ Assets/Scripts/Movemunt.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movemunt : MonoBehaviour
+{
+    public float spd = 5;
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        Vector3 direct = new Vector3(h, v, 0);
+        transform.position = transform.position + direct * spd * Time.deltaTime;
+    }
+}
