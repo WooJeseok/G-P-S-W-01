478048dd691557af23a8201f2b5e0edab92ef674
Jieun <citty3993jin@gmail.com>
2026-03-25 15:46:55 +0900

    22159016_우제석
    0325프로젝트

---
+++ Assets/Scripts/Monster.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster : MonoBehaviour
+{
+    public float spd = 1.0f;
+    public GameObject target;
+    Vector3 direct = Vector3.down;
+    // Update is called once per frame
+    private void Start()
+    {
+        int rndNum = Random.Range(0, 10);
+        if (rndNum % 3 == 0 && target != null)
+        {
+            direct = target.transform.position - transform.position;
+            direct.Normalize();
+        }
+    }
+    void Update()
+    {
+        transform.position = transform.position + direct * spd * Time.deltaTime;
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(collision.gameObject);
+        Destroy(gameObject);
+    }
+}
