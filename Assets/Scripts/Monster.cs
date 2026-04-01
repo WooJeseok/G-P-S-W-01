dc3dbb33d6a2b38bae7e86c4df5e4bc07baea79d
Jieun <citty3993jin@gmail.com>
2026-04-01 16:03:12 +0900

    260401

--- Assets/Scripts/Monster.cs
+++ Assets/Scripts/Monster.cs
@@ -7,9 +7,11 @@ public class Monster : MonoBehaviour
     public float spd = 1.0f;
     public GameObject target;
     Vector3 direct = Vector3.down;
+    public GameObject prefabsExplosion;
     // Update is called once per frame
     private void Start()
     {
+        target = GameObject.Find("Character");
         int rndNum = Random.Range(0, 10);
         if (rndNum % 3 == 0 && target != null)
         {
@@ -23,6 +25,8 @@ public class Monster : MonoBehaviour
     }
     private void OnCollisionEnter(Collision collision)
     {
+        GameObject explosionObi = Instantiate(prefabsExplosion);
+        explosionObi.transform.position = transform.position;
         Destroy(collision.gameObject);
         Destroy(gameObject);
     }
